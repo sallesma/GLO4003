5f23e57a2bae559678410f4c76c08e3919c6fbcf
gaetan-deturche <gaetan.deturche@gmail.com>
2013-10-10 11:27:33 -0400

    Add file access, only saving for the moment. (need to add XOM library and JRE 7 to work)

---
+++ src/main/java/database/FileAcces.java
@@ -0,0 +1,82 @@
+package database;
+
+
+import nu.xom.*;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.*;
+import java.util.Hashtable;
+
+
+
+public class FileAcces {
+	
+	private Hashtable<String, String> fileName = new Hashtable<String, String>();
+
+
+	public FileAcces() {
+		fileName.put("User", "user.xml");
+		fileName.put("Match", "match.xml");
+		fileName.put("", "association.xml");
+	}
+
+	public void save(Element elem, String objectName) throws IOException, ValidityException, ParsingException
+	{
+		
+		Path p = Paths.get(fileName.get(objectName));
+		
+		Element elementId = elem.getFirstChildElement("ID");
+		
+		Builder parser = new Builder();
+		Document doc;
+		if(Files.exists(p))
+		{
+			doc = parser.build(p.toFile());
+			Elements nodes = doc.getRootElement().getChildElements();
+			
+			int i = 0;
+			while (i < nodes.size())
+			{
+				if(nodes.get(i).getChild(1).getValue().equals(elementId.getValue()))
+				{
+					nodes.get(i).detach();
+					break;
+				}
+				i++;
+			}
+			doc.getRootElement().appendChild(elem);
+			
+			Files.delete(p);
+		}
+		else
+		{
+			Element root = new Element(objectName + "s");
+			doc = new Document(root);
+			root.appendChild(elem);
+			
+		}
+	
+		
+		Files.createFile(p);
+		
+		
+		OutputStream out = Files.newOutputStream(p);
+		
+		
+		Serializer serializer = new Serializer(out, "ISO-8859-1");
+        serializer.setIndent(4);
+        serializer.setMaxLength(64);
+        serializer.write(doc);
+		
+	}
+	
+	/*
+	public Element load(String objectName, int id)
+	{
+		Element elem = new Element();
+		Path p = Paths.get(fileName.get(objectName));
+		
+		return elem;
+	}//*/
+}

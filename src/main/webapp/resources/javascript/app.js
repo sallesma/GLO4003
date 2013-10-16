85c8f475048d5ea8a56ca5940cfdf5fec67f19b9
TitouanR <titouan.rossier@gmail.com>
2013-10-15 22:36:15 -0400

    Dynamic filters on MatchList + Replace MatchController with the good version

--- src/main/webapp/resources/javascript/app.js
+++ src/main/webapp/resources/javascript/app.js
@@ -6,3 +6,17 @@ $('#fromDate').datepicker({
 
 $('#toDate').datepicker({
 });
+
+//------------  Filter Form Actions --------------
+
+$('.filterSelect').change(function() { 
+	$( '#filterForm').submit();
+});
+
+$('.removeFilter').click(function() {
+	var inputToClear = $(this).parent().next('input[type=hidden]');
+	inputToClear.val('');
+	$( '#filterForm').submit();
+})
+
+
